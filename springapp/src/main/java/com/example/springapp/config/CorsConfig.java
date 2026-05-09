0f2c9867089a1f0047a3fbc9b52fb47322dd3b45
jeevi-05 <jeevithasakthivel05@gmail.com>
2026-05-09 13:27:56 +0530

    Fixed CORS for deployed frontend

--- springapp/src/main/java/com/example/springapp/config/CorsConfig.java
+++ springapp/src/main/java/com/example/springapp/config/CorsConfig.java
@@ -13,14 +13,31 @@ public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://workorbit-frontend.onrender.com"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
         config.setAllowedHeaders(List.of("*"));
+
         config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
-        return   source;
+
+        return source;
     }
 }
